d97eaee06e9da8cc8cdd3ea03f29571a80c73533
Sabrina Stangler <32472795+sabrina-stangler@users.noreply.github.com>
2022-01-06 17:08:28 -0600

    Add first post, remove default template stuff (#2)
    * Add first post, remove default template stuff
    
    * Fix bio link

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
+        src="../images/profile-pic.jpg"
         width={50}
         height={50}
         quality={95}
@@ -46,8 +46,8 @@ const Bio = () => {
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+          <a href="https://www.youtube.com/channel/UCaMedM3OImbFAHkavO4rfNA">
+            I also make videos on YouTube
           </a>
         </p>
       )}
