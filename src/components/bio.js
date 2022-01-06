91420e3776b7bf9ffc4485101487a6bafe00ed31
Sabrina Stangler <sabrinastangler51@gmail.com>
2022-01-06 17:08:17 -0600

    Fix bio link

--- src/components/bio.js
+++ src/components/bio.js
@@ -46,7 +46,7 @@ const Bio = () => {
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          <a href={`${social?.youtube || ``}`}>
+          <a href="https://www.youtube.com/channel/UCaMedM3OImbFAHkavO4rfNA">
             I also make videos on YouTube
           </a>
         </p>
