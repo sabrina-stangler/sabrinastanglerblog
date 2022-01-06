f4b1e5067a0c791b8a08d06ad490093129ecb9e2
Sabrina Stangler <32472795+sabrina-stangler@users.noreply.github.com>
2022-01-06 16:11:05 -0600

    First change (#1)
    * First commit
    
    * Second commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Sabrina's Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Sabrina Stangler`,
+      summary: `who lives in Milwaukee, WI and has cats`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
